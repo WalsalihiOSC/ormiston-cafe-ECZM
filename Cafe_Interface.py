f01cd05cfb3adad46217e6fd567161e9ef8cbd3c
Ella Ma <84163020+ECZM@users.noreply.github.com>
2022-06-13 21:59:05 +1200

    Update file - Cafe_Interface.py
    Created a class - CafeOrder, for getting details from text file
    Created next function.

--- Cafe_Interface.py
+++ Cafe_Interface.py
@@ -1,13 +1,18 @@
 """This program is about creating a cafe interface for students to make orders and the cafe manager to track student orders"""
 
 from tkinter import *
+#import tkinter.font as font
+#from PIL import ImageTk, Image
 from tkinter.scrolledtext import *
-
+#from tkinter import Text, Tk
 
 class CafeInterface:
     def __init__(self, parent):
+        self.mainframe = Frame(parent, height = 600, width = 1000)
+        self.mainframe.grid(column = 0, row = 0)
+        self.mainframe.grid_propagate(0)
 
-        self.fCoffee = Frame(parent, height = 600, width = 1000)
+        self.fCoffee = Frame(self.mainframe, height = 600, width = 1000)
         self.fCoffee.grid(column = 0, row = 0)
         self.fCoffee.grid_propagate(0)
 
@@ -83,17 +88,18 @@ class CafeInterface:
         self.l7.place(x = 640, y = 80)
 
         self.coffeeList = []
-        self.coffee = self.menuslides[0].orders
-        self.coffees = self.coffee.split(",")
+        self.coffee = self.menuslides[0].category
+        self.coffees = self.coffee.split("#")
         for item in self.coffees:
             self.coffeeList.append(item)
             
         self.v = StringVar()
         self.v.set("0")
-        self.coffeemenu = self.coffeeList
+        self.coffeepriceInt = [3, 3, 3.50, 4.20, 4.50, 5.25, 4.50, 5.00]
         self.cb1 = []
-        for i in range(len(self.coffeemenu)):
-            self.cb1.append(Checkbutton(self.f3, variable = self.v, text = self.coffeemenu[i]))
+        for i in range(len(self.coffeeList)):
+            self.k = Checkbutton(self.f3, variable = self.v, text = self.coffeeList[i])
+            self.cb1.append(self.k)
             self.cb1[i].grid(column = 0, sticky = W, ipadx = 10, ipady = 5)
 
         self.f4 = Frame(self.c3)
@@ -101,14 +107,13 @@ class CafeInterface:
 
         self.costList = []
         self.cost = self.menuslides[0].prices
-        self.costs = self.cost.split(",")
+        self.costs = self.cost.split("#")
         for item2 in self.costs:
             self.costList.append(item2)
-
-        self.coffeeprices = self.costList
+        
         self.pricelabels = []
-        for i in range(len(self.coffeeprices)):
-            self.pricelabels.append(Label(self.f4, text = self.coffeeprices[i]))
+        for i in range(len(self.costList)):
+            self.pricelabels.append(Label(self.f4, text = self.costList[i]))
             self.pricelabels[i].grid(column = 0, sticky = NE, ipadx = 10, ipady = 4)
         
         self.f5 = Frame(self.c3)
@@ -121,21 +126,61 @@ class CafeInterface:
             self.e1.append(Entry(self.f5, width = 1, bg = "#e3cbb3", font = "None, 12"))
             self.e1[i].grid(column = 0, sticky = W, ipadx = 10, ipady = 2)
 
-        self.b1 = Button(self.c3, text = ">")
+        self.b1 = Button(self.c3, text = ">", command = self.next)
         self.b1.place(x = 350, y = 400)
 
         self.b2 = Button(self.c3, text = "<")
         self.b2.place(x = 330, y = 400)
+    
+    def next(self):
+        self.f3.destroy()
+        self.F3 = Frame(self.c3)
+        self.F3.place(x = 320, y = 100)
+        self.f4.destroy()
+        self.F4 = Frame(self.c3)
+        self.F4.place(x = 570, y = 100)
+
+        self.target += 1
+        if self.target >= len(self.menuslides):
+            self.target = 0
+        
+        self.c2.mainbanner.configure(file = self.menuslides[self.target].banner)
+        self.c3.mainheader.configure(file = self.menuslides[self.target].header)
+
+        self.menuList = []
+        self.menu = self.menuslides[self.target].category
+        self.menus = self.menu.split("#")
+        for menuitem in self.menus:
+            self.menuList.append(menuitem)
+   
+        self.V = StringVar()
+        self.V.set("0")
+        self.category = self.menuList
+        
+        self.Cb1 = []
+        for i in range(len(self.category)):
+            self.Cb1.append(Checkbutton(self.F3, variable = self.V, text = self.category[i]))
+            self.Cb1[i].grid(column = 0, sticky = W, ipadx = 10, ipady = 5)
+        
+        self.allcostList = []
+        self.allcost = self.menuslides[self.target].prices
+        self.allcosts = self.allcost.split("#")
+        for items in self.allcosts:
+            self.allcostList.append(items)
+
+        self.allprices = self.allcostList
+        self.allpricelabels = []
+        for i in range(len(self.allprices)):
+            self.allpricelabels.append(Label(self.F4, text = self.allprices[i]))
+            self.allpricelabels[i].grid(column = 0, sticky = NE, ipadx = 10, ipady = 4)
 
 
 class CafeOrders:
-    def __init__(self, banner, header, orders, prices):
+    def __init__(self, banner, header, category, prices):
         self.banner = banner
         self.header = header
-        self.orders = orders
+        self.category = category
         self.prices = prices
-    
-
 
 
 #Main Routine
